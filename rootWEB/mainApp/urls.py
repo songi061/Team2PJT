b683ef9b12d4e258517134e153c1e7b496a31e40
hskim <rjdls2549@donga.ac.kr>
2023-12-18 16:49:50 +0900

    root error

--- rootWEB/mainApp/urls.py
+++ rootWEB/mainApp/urls.py
@@ -5,11 +5,11 @@ from mainApp import views
 urlpatterns = [
     #http://127.0.0.1:8000/
     path("", views.index, name = 'index'),
-    path("scalp/", views.scalp),
-    path("shampoo/", views.shampoo),
-    path("my/", views.my),
-    path("login/", views.login),
-    path("join/", views.join),
+    path("scalp/", views.scalp, name = 'scalp'),
+    path("shampoo/", views.shampoo, name = 'shampoo'),
+    path("my/", views.my, name = 'my'),
+    path("login/", views.login, name = 'login'),
+    path("join/", views.join, name = 'join'),
     path("upload/", views.upload),
     path('shampooButton/', views.shampooButton, name='shampooButton'),
     path('loss/', views.loss),
